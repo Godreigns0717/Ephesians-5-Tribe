9af4abdbfbab2293e0afcffc93a01da5cb6462b8
Akintunde Oluwajoba <oluwajobaakintunde0@gmail.com>
2023-11-25 20:06:07 +0100

    pushing to github repositry

--- src/components/Pages/Donate/Donate.js
+++ src/components/Pages/Donate/Donate.js
@@ -1,13 +1,26 @@
 import React from "react";
 import './Donate.css'
 import Navbar from "../../Navbar";
+import DonationPage from "./DonationPage";
 
 const Donate = () => {
     return(
         <div className="Donate">
             <Navbar />
             <div className="donate-card">
+                <div className="donate-card_text">
+                    <h2>You Can Pay Through:</h2>
 
+                    <h3>Opay</h3>
+                    <p>9117298808</p>
+
+                    <h3>Stanbic</h3>
+                    <p>9117298808</p>
+                </div>
+            </div>
+            <DonationPage />
+            <div className="donate-form">
+                
             </div>
         </div>
     )
